8a215bc9a7e1088bac8b177a4ad3cff9bfd05bdf
Shane Steszyn <shane@steszyn.ca>
2015-06-25 15:43:11 -0400

    GPIO beginnings

--- app/routes/api/gpio/index.js
+++ app/routes/api/gpio/index.js
@@ -1,6 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 var gpio = require('rpi-gpio');
 
-gpio.on('change', function(channel, value) {
-    console.log('Channel ' + channel + ' value is now ' + value);
+var router = express.Router();
+
+var relayPump = {pin: 2, value: ''};
+var relayS1 = {pin: 3, value: ''};
+var relayS2 = {pin: 17, value: ''};
+var relayS3 = {pin: 27, value: ''};
+
+var pinList = [relayPump, relayS1, relayS2, relayS3];
+var builder = {};
+builder.status = failed;
+
+router.get('/', function(req, res){
+  for (var i = 0; i < pinList.length; i++){
+    gpio.setup(pinList[i].pin, gpio.DIR_IN, readInput);
+    gpio.read(pinList[i].pin, function(err, value) {
+      console.log('The value is ' + value);
+      pinList[i].value = value;
+    });
+
+    gpio.destroy();
+  }
+
+  builder.status="success";
+  builder.gpio = pinList;
+  res.json(builder);
 });
-gpio.setup(7, gpio.DIR_IN);
+
+module.exports = router;
