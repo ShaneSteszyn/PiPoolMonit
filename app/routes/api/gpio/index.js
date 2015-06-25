082423a8fcba1242bc9ca8e46f69932649973b4b
Shane Steszyn <shane@steszyn.ca>
2015-06-25 16:43:40 -0400

    GPIO toggles?

--- app/routes/api/gpio/index.js
+++ app/routes/api/gpio/index.js
@@ -13,19 +13,42 @@ var pinList = [relayPump, relayS1, relayS2, relayS3];
 var builder = {};
 builder.status = failed;
 
+readInput = function(index){
+  gpio.read(pinList[index].pin, function(err, value) {
+    if (err) throw err;
+    console.log('The value is ' + value);
+    pinList[index].value = value;
+  });
+};
+
+write = function(data){
+  gpio.write(pinList[i].pin, data.value, function(err) {
+    if (err) throw err;
+    console.log('Written to pin');
+  });
+};
+
 router.get('/', function(req, res){
   for (var i = 0; i < pinList.length; i++){
-    gpio.setup(pinList[i].pin, gpio.DIR_IN, readInput);
-    gpio.read(pinList[i].pin, function(err, value) {
-      console.log('The value is ' + value);
-      pinList[i].value = value;
-    });
-
-    gpio.destroy();
+    gpio.setup(pinList[i].pin, gpio.DIR_IN, readInput(i));    
   }
 
+  gpio.destroy();
   builder.status="success";
   builder.gpio = pinList;
+  res.json(builder);
+});
+
+router.post('/', function(req, res){
+  // data === {pin: Number, value: String/Number/Bool}
+  var data = req.body.data; //Data to possible say which pin and value to apply to it.
+
+  gpio.setup(data.pin, gpio.DIR_OUT, write(data));
+
+  gpio.destroy();
+
+  builder.status="success";
+  builder.message = data.value + ' written to pin!';
   res.json(builder);
 });
 
